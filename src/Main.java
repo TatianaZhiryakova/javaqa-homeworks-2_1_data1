8f347eb59f5bbcdd62ec09addc4ed3b003931ba7
TatianaZhiryakova <tatjana_tv@list.ru>
2021-10-21 00:38:26 +0300

    added project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       int price = 2510;
+       int oneMile = 20;
+       int miles;
+       miles = price / oneMile;
+        System.out.println(miles);
+    }
+}
